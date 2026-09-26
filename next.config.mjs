f3ca81f815ea79f3c1c96d21613c5dade502b892
Shweta Kedare <shwetakedare96gitproject@gmail.com>
2026-09-26 17:58:30 +0530

    fix: set default DNS result order to ipv4first

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
